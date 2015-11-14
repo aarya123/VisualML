34df0fb89c537b4a52b9d1c7a48b4b34495b5ce8
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-11-14 18:04:34 -0500

    Update centroids one last time

--- js/colorutils.js
+++ js/colorutils.js
@@ -1,38 +1,23 @@
-/**
- * HSV to RGB color conversion
- *
- * H runs from 0 to 360 degrees
- * S and V run from 0 to 100
- * 
- * Ported from the excellent java algorithm by Eugene Vishnevsky at:
- * http://www.cs.rit.edu/~ncs/color/t_convert.html
- */
 function hsvToRgb(h, s, v) {
 	var r, g, b;
 	var i;
 	var f, p, q, t;
 	
-	// Make sure our arguments stay in-range
 	h = Math.max(0, Math.min(360, h));
 	s = Math.max(0, Math.min(100, s));
 	v = Math.max(0, Math.min(100, v));
 	
-	// We accept saturation and value arguments from 0 to 100 because that's
-	// how Photoshop represents those values. Internally, however, the
-	// saturation and value are calculated from a range of 0 to 1. We make
-	// That conversion here.
 	s /= 100;
 	v /= 100;
 	
 	if(s == 0) {
-		// Achromatic (grey)
 		r = g = b = v;
 		return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 	}
 	
-	h /= 60; // sector 0 to 5
+	h /= 60;
 	i = Math.floor(h);
-	f = h - i; // factorial part of h
+	f = h - i;
 	p = v * (1 - s);
 	q = v * (1 - s * f);
 	t = v * (1 - s * (1 - f));
@@ -68,7 +53,7 @@ function hsvToRgb(h, s, v) {
 			b = v;
 			break;
 			
-		default: // case 5:
+		default:
 			r = v;
 			g = p;
 			b = q;
@@ -76,3 +61,12 @@ function hsvToRgb(h, s, v) {
 	
 	return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
+
+function nRandomColors(total) {
+        var i = 360 / (total - 1);
+        var r = [];
+        for (var x = 0; x < total; x++) {
+            r.push(hsvToRgb(i * x, 100, 100));
+        }
+        return r;
+    }
